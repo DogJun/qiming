be8e347a4137126c38fbbfcd956ed5a9194191d1
DogJun <498343874@qq.com>
2018-09-13 23:55:30 +0800

    :boom: 完善结果页面和详情页面

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,3 +3,5 @@ import fetch from './fetch'
 export const wxLogin = (data = {}, options = {}) => fetch.post('/wechat/user/login', data, options)
 // 起名
 export const getName = (data = {}, options = {}) => fetch.post('/api/qiming/queryName', data, options)
+// 名字详情
+export const getDetailName = (data = {}, options = {}) => fetch.post('/api/qiming/getDetailName', data, options)

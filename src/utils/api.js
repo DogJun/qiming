3de0afbfcb1365418070091d41f6961106e0274d
DogJun <498343874@qq.com>
2018-09-17 23:03:04 +0800

    :boom: 新增起名记录

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,3 +5,11 @@ export const wxLogin = (data = {}, options = {}) => fetch.post('/wechat/user/log
 export const getName = (data = {}, options = {}) => fetch.post('/api/qiming/queryName', data, options)
 // 名字详情
 export const getDetailName = (data = {}, options = {}) => fetch.post('/api/qiming/getDetailName', data, options)
+// 轮播图
+export const getAd = (data = {}, options = {}) => fetch.post('/api/qiming/getAd', data, options)
+// 常见问题
+export const getQa = (data = {}, options = {}) => fetch.post('/api/qiming/getHelp', data, options)
+// 反馈意见
+export const getFeedBack = (data = {}, options = {}) => fetch.post('/api/qiming/saveQuestion', data, options)
+// 获取起名记录
+export const getRecord = (data = {}, options = {}) => fetch.post('/api/qiming/getQimingRecord', data, options)
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -2,9 +2,12 @@ import wepy from 'wepy'
 // import config from './config'
 const BASE_URL = 'https://qiming.jiaohuanziyuan.xin'
 async function fetch (options = {}) {
-  if (!options.hideBarLoading) {
-    wepy.showNavigationBarLoading()
-  }
+  // if (!options.hideBarLoading) {
+  //   wepy.showNavigationBarLoading()
+  // }
+  wepy.showLoading({
+    title: '拼命加载中...'
+  })
   let token = wepy.getStorageSync('token') || ''
   if (token) {
     options.data = options.data || {}
@@ -15,29 +18,27 @@ async function fetch (options = {}) {
   return wepy.request({
     ...options
   }).then((res) => {
-    wepy.hideNavigationBarLoading()
+    // wepy.hideNavigationBarLoading()
+    wepy.hideLoading()
     console.log(res)
     const response = res.data
 
-    if (response && response.success === false) {
+    if (response && response.code !== 0) {
       return Promise.reject(response)
     }
     return response
     // return res.data
   }).catch((e) => {
-    console.log(JSON.stringify(e.message))
-    wepy.hideNavigationBarLoading()
-
+    wepy.hideLoading()
+    console.log(JSON.stringify(e))
+    // 微信的报错信息或自己服务器的报错信息
+    let err = e.errMsg || e.msg
+    // wepy.hideNavigationBarLoading()
     if (!options.noShowModal) {
       wepy.showModal({
         title: '',
-        content: e.message,
+        content: err,
         showCancel: false
-      }).then(res => {
-        // 非法请求，重新获取token
-        if (e.errorCode === 10010) {
-          refreshToken()
-        }
       })
     }
     return Promise.reject(e)
